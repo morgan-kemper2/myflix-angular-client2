76213dac61c9c5bed210818c8a0a2c2232ac4d14
Morgan Kemper <morgankemper@Morgans-Air.attlocal.net>
2021-03-31 13:17:15 -0500

    added registration and login views

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,50 @@
+// src/app/user-registration-form/user-registration-form.component.ts
+import { Component, OnInit, Input } from '@angular/core';
+
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls we created in 6.2
+import { LoginUserService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '' };
+
+constructor(
+    public fetchApiData: LoginUserService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+ngOnInit(): void {
+}
+
+// This is the function responsible for sending the form inputs to the backend
+loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+  // Logic for a successful user registration goes here! (To be implemented)
+     this.dialogRef.close(); // This will close the modal on success!
+     localStorage.setItem('user', response.user.Username);
+     localStorage.setItem('token', response.token);
+     console.log(response);
+     this.snackBar.open('user sucessfully logged in', 'OK', {
+        duration: 2000
+     });
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+  }
